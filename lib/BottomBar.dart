05d30d1ecf45c4b99f87113df52ba01753b84c51
KerKerook <kerkerook@gmail.com>
2023-06-10 18:47:19 +0330

    add Bottom bar

---
+++ lib/BottomBar.dart
@@ -0,0 +1,103 @@
+import 'package:untitled/HomePage.dart';
+import 'package:untitled/log.dart';
+import 'package:flutter/material.dart';
+import 'package:untitled/triplist.dart';
+
+import 'accountInfo.dart';
+import 'moneypage.dart';
+
+class Bar extends StatefulWidget {
+  final int index;
+  const Bar({Key? key, required this.index}) : super(key: key);
+
+  @override
+  State<Bar> createState() => _BarState();
+}
+
+class _BarState extends State<Bar> {
+  void hompage() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => homepage()),
+    );
+  }
+
+  void userInformationTapped() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => AccountInfoPage(accountMoney: 220,birthdayValue: "aban",email: "ali@gmail.com",genderValue: "male",meliCodeValue: "22837281",name: "ali",nameValue: "aliuchiha",)),
+    );
+  }
+  List<Flight> flights =[
+    Flight(time: "15 PM", cost: "2500", company: "iran air", shomare_sefaresh: "4024130", date: DateTime.now())
+  ];
+  void flightListonTapped() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => TripsListPage(flights: flights)),
+    );
+  }
+  List<Tarakonesh> lisssst = [Tarakonesh(kind: "داخلی", increase: true, codePaygiri: "015865", cost: 456456),Tarakonesh(kind: "خارجی", increase: false, codePaygiri: "01584565", cost: 85856)];
+  void budgetonTapped() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) =>  MoneyPage(initialBalance: 2000, tarakoneshList: lisssst,)),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.deepOrangeAccent,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.indigo.withOpacity(0.5),
+            blurRadius: 5,
+            offset: Offset(0, -3),
+          ),
+        ],
+      ),
+      child: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'صفحه اصلی',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.info),
+            label: 'حساب کاربری',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.flight),
+            label: 'لیست سفر ها',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.money),
+            label: 'موجودی و تراکنش ها',
+          ),
+        ],
+        currentIndex: widget.index,
+        unselectedItemColor: Colors.blueGrey[400],
+        selectedItemColor: Colors.amber[600],
+        onTap: (index) {
+          switch (index) {
+            case 0:
+              hompage();
+              break;
+            case 1:
+              userInformationTapped();
+              break;
+            case 2:
+              flightListonTapped();
+              break;
+            case 3:
+              budgetonTapped();
+              break;
+          }
+        },
+      ),
+    );
+  }
+}
+
